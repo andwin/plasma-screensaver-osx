861951e507af7453eb4c4443c8072355dd0611dd
Anders Windelhed <anders.windelhed@valtech.se>
2016-01-05 22:37:58 +0100

    Initial Commit

---
+++ PlasmaScreensaver/PlasmaScreensaverView.h
@@ -0,0 +1,13 @@
+//
+//  PlasmaScreensaverView.h
+//  PlasmaScreensaver
+//
+//  Created by Anders Windelhed on 05/01/16.
+//  Copyright © 2016 anders. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface PlasmaScreensaverView : ScreenSaverView
+
+@end
---
+++ PlasmaScreensaver/PlasmaScreensaverView.m
@@ -0,0 +1,52 @@
+//
+//  PlasmaScreensaverView.m
+//  PlasmaScreensaver
+//
+//  Created by Anders Windelhed on 05/01/16.
+//  Copyright © 2016 anders. All rights reserved.
+//
+
+#import "PlasmaScreensaverView.h"
+
+@implementation PlasmaScreensaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
